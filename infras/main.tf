461493b537e5684e788febd1a113f3457f0fe45e
samsonbaruwa <samsonbaruwa91@gmail.com>
2023-02-14 15:37:33 +0000

    newfile

---
+++ infras/main.tf
@@ -0,0 +1,12 @@
+# Create EC2 instance
+resource "aws_instance" "instancepostgres" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.ssh_key_name
+  vpc_security_group_ids = [aws_security_group.example.id]
+  subnet_id     = element(var.subnets, count.index)
+  count         = length(var.subnets)
+
+  tags = {
+    Name = "${var.instance_name}-${count.index}"
+  }
---
+++ infras/variable.tf
@@ -0,0 +1,35 @@
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami" {
+  default = "ami-00eeedc4036573771" # Ubuntu 20.04 LTS
+}
+
+variable "ssh_key_name" {
+  default = "my-ssh-key"
+}
+
+variable "vpc_id" {
+  default = "vpc-0abeadba9e4b656a8"
+}
+
+variable "subnets" {
+  default = ["subnet-0e6be677dc7894da4", "subnet-028910f95fcfce01a"]
+}
+
+variable "instance_name" {
+  default = "instancepostgres"
+  }
+
+variable "replication_slot_name" {
+  default = "replication_slot"
+}
+
+variable "primary_instance_private_ip" {
+  default = "172.31.1.16" # Replace with primary instance private IP
+}
+
+variable "backup_bucket_name" {
+  default = "my-backup-bucket"
+}
