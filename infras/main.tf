eb13543011b9255f1c06371a8c8524a8f302d74b
samsonbaruwa <samsonbaruwa91@gmail.com>
2023-02-14 17:19:02 +0000

    gg

--- infras/main.tf
+++ infras/main.tf
@@ -3,7 +3,6 @@ resource "aws_instance" "instancepostgres" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.ssh_key_name
-  vpc_security_group_ids = [aws_security_group.instancepostgres.id]
   subnet_id     = element(var.subnets, count.index)
   count         = length(var.subnets)
 
