43f47600d94d720cfc15f1488a9995d1e24d1d89
samsonbaruwa <samsonbaruwa91@gmail.com>
2023-02-14 22:16:19 +0000

    edi4

--- infras/provider.tf
+++ infras/provider.tf
@@ -5,11 +5,6 @@ terraform {
       version = "~> 4.0"
     }
   }
-  backend "s3" {
-    bucket         = "worktest1123"
-    key            = "network/terraform.tfstate"
-    region         = "us-east-1"
-  }
 }
 
 provider "aws" {
