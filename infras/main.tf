b515575c744af0a57a9d7706741081ce88f31a7b
samsonbaruwa <samsonbaruwa91@gmail.com>
2023-02-15 13:03:40 +0000

    key

--- infras/main.tf
+++ infras/main.tf
@@ -4,10 +4,15 @@ resource "aws_instance" "instancepostgres" {
   instance_type = var.instance_type
   subnet_id     = element(var.subnets, count.index)
   count         = length(var.subnets)
-
   user_data = filebase64("${path.module}/bootstrap.sh")
 
   tags = {
     Name = "${var.instance_name}-${count.index}"
   }
+
+  resource "aws_key_pair" "samskey" {
+  key_name   = "my-key-pair"
+  public_key = var.aws_key_pair
+}
+
 }
